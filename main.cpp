9e2a7b63306131b48523683c520f6cf7fe69eb22
Adrijaned <adrijaned@gmail.com>
2019-04-22 00:40:29 +0200

    Projection, view, transform matrices

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ int main() {
   sf::ContextSettings settings;
   settings.majorVersion = 3;
   settings.minorVersion = 3;
+  settings.depthBits = 32;
   sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, settings);
   glewInit();
+  glEnable(GL_DEPTH_TEST);
 
   window.setVerticalSyncEnabled(true);
   window.setActive(true);
@@ -24,6 +26,11 @@ int main() {
   glm::mat4 trans = glm::mat4(1.0f);
   trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
   trans = glm::scale(trans, glm::vec3(1.5, 1.5, 1.5));
+  glm::mat4 projectionMatrix = glm::perspective(glm::quarter_pi<float>(), 4.0f / 3.0f, 0.1f, 100.0f);
+  glm::mat4 view = glm::mat4(1.0f);
+  // note that we're translating the scene in the reverse direction of where we want to move
+  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -1.3f));
+  shaderProgram.activate().setUniform("projection", projectionMatrix).setUniform("view", view);
 
   bool running = true;
   while (running) {
@@ -33,11 +40,13 @@ int main() {
         running = false;
       } else if (event.type == sf::Event::Resized) {
         glViewport(0, 0, event.size.width, event.size.height);
+        projectionMatrix = glm::perspective(glm::quarter_pi<float>(), ((float) event.size.width) / ((float) event.size.height), 0.1f, 100.0f);
+        shaderProgram.activate().setUniform("projection", projectionMatrix);
       }
     }
 
     glClearColor(0.1, 0.3, 0.2, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // NOLINT(hicpp-signed-bitwise)
     shaderProgram.activate();
     shaderProgram.setUniform("transform", trans);
     goxelObject.drawBuffers();
--- shaders/vBasic.glsl
+++ shaders/vBasic.glsl
@@ -6,8 +6,11 @@ layout (location = 1) in vec3 vColor;
 out vec3 fColor;
 
 uniform mat4 transform;
+uniform mat4 projection;
+uniform mat4 view;
 
 void main() {
-    gl_Position = transform * vec4(inPos, 1.0);
+    gl_Position = projection *view* transform * vec4(inPos, 1.0);
+//    gl_Position = transform*vec4(inPos, 1.0f);
     fColor = vColor;
 }
